9dafab93c6118ee6c447f5368e876aa661e5a968
cr3wcr3w <racelisjk@gmail.com>
2024-12-18 13:01:36 +0800

    fix: added build output dir

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  distDir: 'build',
 }
 
 export default withPayload(nextConfig)

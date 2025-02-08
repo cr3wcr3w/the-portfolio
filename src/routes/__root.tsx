54bcbf58daeba1862eca43c40fa1f06df9f4394e
cr3wcr3w <racelisjk@gmail.com>
2025-02-08 19:41:37 +0800

    fix: remove custom 404 component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import AnimationWrapper from "../shared/components/animation-wrapper";
 import Navigation from "../shared/components/navigation";
 import Footer from "../shared/components/footer";
-import NotFound from "../shared/components/not-found/not-found";
+// import NotFound from "../shared/components/not-found/not-found";
 
 export const Route = createRootRoute({
   component: () => (
@@ -14,5 +14,5 @@ export const Route = createRootRoute({
       {/* <TanStackRouterDevtools /> */}
     </AnimationWrapper>
   ),
-  notFoundComponent: () => <NotFound />,
+  // notFoundComponent: () => <NotFound />,
 });

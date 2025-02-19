610b3228857636623e860d505acf423c6f5ae2d1
cr3wcr3w <racelisjk@gmail.com>
2025-02-20 04:23:28 +0800

    test: broke formatting

--- src/shared/components/footer/index.tsx
+++ src/shared/components/footer/index.tsx
@@ -2,9 +2,9 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="container mx-auto my-8 max-w-2xl px-4">
-      <p className="text-center text-sm text-slate-400">
-        Build by me ©{currentYear}
-      </p>
+                <p className="text-center text-sm text-slate-400">
+                  Build by me ©{currentYear}
+                </p>
     </footer>
   );
 }
